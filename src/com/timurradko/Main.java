3fa92c279baff3d4bcb3e10faf5a6c4e2fa51df8
Timur Radko <timurradko@gmail.com>
2020-02-25 12:10:15 +0300

    Simple File Manager

---
+++ src/com/timurradko/Main.java
@@ -0,0 +1,8 @@
+package com.timurradko;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/timurradko/fileExmpl/FileGid.java
@@ -0,0 +1,116 @@
+package com.timurradko.fileExmpl;
+
+import java.io.File;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class FileGid {
+    private StringBuilder currentPath = new StringBuilder();
+    private Scanner scanner = new Scanner(System.in);
+
+    public File pave() {
+        while (true) {
+            try {
+                choiceRoot();
+                printCurFileContent();
+                run();
+            } catch (Exception e) {
+                System.out.println(e.getMessage());
+                currentPath = new StringBuilder();
+            }
+        }
+    }
+
+    private File run() {
+        while (true) {
+            String input = scanner.nextLine();
+            implInput(input);
+            printCurFileContent();
+            deleteFile(input);
+        }
+    }
+
+    private void implInput(String input) {
+        checkExit(input);
+        input = input.trim();
+        if (input.startsWith("cd ")) {
+            input = input.substring(3);
+            input = input.trim();
+            String[] pathParts = input.split("\\\\");
+            for (String path : pathParts) {
+                if (path.equals("..")) {
+                    int slashIndex = currentPath.lastIndexOf("\\");
+                    if (slashIndex != -1) {
+                        currentPath.delete(slashIndex - 1, currentPath.length());
+                        slashIndex = currentPath.lastIndexOf("\\");
+                        currentPath.delete(slashIndex + 1, currentPath.length());
+                    } else {
+                        choiceRoot();
+                    }
+                } else {
+                    currentPath.append(path).append("\\");
+                }
+            }
+        }
+    }
+
+    private void deleteFile(String input) {
+        checkExit(input);
+        input = input.trim();
+        if (input.startsWith("del ")) {
+            input = input.substring(4);
+            input = input.trim();
+            File file = new File(currentPath.toString());
+            File[] files = file.listFiles();
+            if (files != null) {
+                for (File innerFile : files) {
+                    String name = innerFile.getName();
+                if (input.equals(name)) {
+                    innerFile.delete();
+                    System.out.println(name + " was deleted");
+                }
+            }
+        }
+    }
+}
+
+    private void choiceRoot() {
+        File[] roots = File.listRoots();
+        System.out.println(Arrays.toString(roots));
+        String input = scanner.nextLine();
+        for (File file : roots) {
+            String path = file.getPath();
+            if (path.startsWith(input) || path.equalsIgnoreCase(input)) {
+                currentPath.append(path);
+                return;
+            }
+        }
+    }
+
+    private void printCurFileContent() {
+        File file = new File(currentPath.toString());
+        if (!file.exists()) {
+            throw new IllegalStateException("The file doesn't exist");
+        }
+        File[] files = file.listFiles();
+        if (files != null) {
+            for (File innerFile : files) {
+                System.out.println(innerFile);
+            }
+        } else {
+            System.out.println("Directory is empty");
+        }
+    }
+
+    private void checkExit(String input) {
+        String s = "exit";
+        if (s.equalsIgnoreCase(input)) {
+            System.exit(0);
+        }
+    }
+
+    public static void main(String[] args) {
+        FileGid gid = new FileGid();
+        gid.pave();
+    }
+}
